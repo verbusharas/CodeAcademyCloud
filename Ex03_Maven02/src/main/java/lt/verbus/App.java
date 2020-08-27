590541fc860578c657dec9bf462aceb260fbc513
verbusharas <sarunas.verbus@gmail.com>
2020-08-27 14:58:13 +0300

    init commit

---
+++ Ex03_Maven02/src/main/java/lt/verbus/App.java
@@ -0,0 +1,25 @@
+package lt.verbus;
+
+import com.github.javafaker.Faker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Faker faker = new Faker();
+
+        String name = faker.name().fullName(); // Miss Samanta Schmidt
+        String firstName = faker.name().firstName(); // Emory
+        String lastName = faker.name().lastName(); // Barton
+
+        String streetAddress = faker.address().streetAddress(); // 60018 Sawayn Brooks Suite 449
+
+        System.out.println(name + "\n" + firstName + "\n" + lastName + "\n" + streetAddress);
+
+    }
+}
