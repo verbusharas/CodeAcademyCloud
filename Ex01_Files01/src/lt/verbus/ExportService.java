f8779968861de2ce42a67b6d7abf6a7e6c9aaf81
verbusharas <sarunas.verbus@gmail.com>
2020-08-27 14:02:42 +0300

    removed asterisk to specify which exact classes are imported. exportSenders and exportReceivers methods merged into reusable exportPeople method.

--- Ex01_Files01/src/lt/verbus/ExportService.java
+++ Ex01_Files01/src/lt/verbus/ExportService.java
@@ -4,15 +4,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 
 public class ExportService {
-    public void exportReceivers(Map<Integer, Person> people, String pathToFile){
+
+    public void exportPeople(Map<Integer, Person> people, String pathToFile, SortOrder order) {
         List<Person> peopleList = new ArrayList<>();
         for (Map.Entry entry : people.entrySet()) {
-            peopleList.add((Person)entry.getValue());
+            peopleList.add((Person) entry.getValue());
         }
-        peopleList.sort(new ComparatorReceived());
+
+        switch (order) {
+            case BY_RECEIVED: peopleList.sort(new ComparatorReceived()); break;
+            case BY_SENT: peopleList.sort(new ComparatorSent()); break;
+        }
+
         Collections.reverse(peopleList);
 
         try (BufferedWriter peopleWriter = Files.newBufferedWriter(Paths.get(pathToFile))) {
@@ -20,30 +30,9 @@ public class ExportService {
                 peopleWriter.write(p.toString());
                 peopleWriter.newLine();
             }
+            System.out.println("List of people sorted by " + order.getSuccessText() + " and exported successfully to file: " + pathToFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println("List of people sorted by -most received money- exported successfully to file: " + pathToFile);
-    }
-    public void exportSenders(Map<Integer, Person> people, String pathToFile){
-        List<Person> peopleList = new ArrayList<>();
-        for (Map.Entry entry : people.entrySet()) {
-            peopleList.add((Person)entry.getValue());
-        }
-
-        peopleList.sort(new ComparatorSent());
-        Collections.reverse(peopleList);
-
-        try (BufferedWriter peopleWriter = Files.newBufferedWriter(Paths.get(pathToFile))) {
-            for (Person p : peopleList) {
-                peopleWriter.write(p.toString());
-                peopleWriter.newLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("List of people sorted by -most sent money- exported successfully to file: " + pathToFile);
     }
 }
