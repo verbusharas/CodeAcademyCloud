6cc617c81455062acd257ae311b68a3c3b39961f
Sarunas Verbus <sarunas@verbus.lt>
2020-08-29 18:14:49 +0300

    init commit

---
+++ Ex_Maven04_Parent/src/main/java/lt/verbus/AppMaven04Parent.java
@@ -0,0 +1,15 @@
+package lt.verbus;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMaven04Parent
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+//        Faker faker = new Faker();
+
+    }
+}
