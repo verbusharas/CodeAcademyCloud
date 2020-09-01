d1c5b7a5ec3f00c5cba7e340f6488a2f9343dece
Sarunas Verbus <sarunas@verbus.lt>
2020-09-01 22:26:31 +0300

    Completed 1 through 3 bullet points

--- Ex_Regex01/src/main/java/lt/verbus/AppRegex01.java
+++ Ex_Regex01/src/main/java/lt/verbus/AppRegex01.java
@@ -2,12 +2,9 @@ package lt.verbus;
 
 /**
  * Hello world!
- *
  */
-public class AppRegex01
-{
-    public static void main( String[] args )
-    {
+public class AppRegex01 {
+    public static void main(String[] args) {
         String tekstas = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
                 "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " +
                 "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
@@ -15,21 +12,28 @@ public class AppRegex01
                 "fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
                 "culpa qui officia deserunt mollit anim id est laborum.";
 
+        // 01
+        System.out.println("----------------------------");
         String newText = tekstas.replaceAll("\\b[lL][a-z]*", "*****");
         // geresnis \b(e|E)[a-zA-Z]*
         System.out.println(newText);
 
+
+        // 02
+        System.out.println("----------------------------");
         String[] tekstoZodziai = tekstas.split(" ");
-        for(String zodis : tekstoZodziai) {
+        for (String zodis : tekstoZodziai) {
             System.out.println(zodis);
         }
 
-        String[] tekstoSakiniai = tekstas.split(".|,");
-        for(String sakinys : tekstoSakiniai) {
+        // 03
+        System.out.println("----------------------------");
+        // Regex explanation: (?<=regexp) - looks behind, but doesn't use,
+        // in other words - finds A preceded by B, or in this case finds SPACE preceded by , OR .
+        String[] tekstoSakiniai = tekstas.split("(?<=[,.])[ ]");
+        for (String sakinys : tekstoSakiniai) {
             System.out.println(sakinys);
         }
-
-
 
     }
 }
