36a568273f1c816c07879d8a46003dedff3b6732
Sarunas Verbus <sarunas@verbus.lt>
2020-11-27 20:24:12 +0200

    cleanup

--- Ex_MongoDb01/src/main/java/lt/verbus/AppMongoDb01.java
+++ Ex_MongoDb01/src/main/java/lt/verbus/AppMongoDb01.java
@@ -1,13 +1,82 @@
 package lt.verbus;
 
-/**
- * Hello world!
- *
- */
-public class AppMongoDb01
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import lt.verbus.model.User;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class AppMongoDb01 {
+
+
+    public static void main(String[] args) {
+
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("LocalDatabase");
+        MongoCollection<Document> collection = mongoDatabase.getCollection("LocalCollection");
+
+        printAllUsers(collection);
+
+        User user1 = new User.Builder(1)
+                .withName("Peter")
+                .withSurname("Griffin")
+                .withEmail("petergriffin@family.com")
+                .atAddress("Quahog")
+                .withPassword("louis999")
+                .build();
+
+        User user2 = new User.Builder(2)
+                .withName("Louis")
+                .withSurname("Griffin")
+                .withEmail("louisGriffin@family.com")
+                .atAddress("Quahog")
+                .withPassword("loveis")
+                .build();
+
+
+        collection.insertOne(makeDocument(user1));
+        collection.insertOne(makeDocument(user2));
+
+        printAllUsers(collection);
+
+        collection.updateOne(eq("id", 1),
+                Updates.set("address", "Quahog 10"));
+
+        printAllUsers(collection);
+        Document document = collection.find(eq("email", "petergriffin@family.com")).first();
+        document.get("name");
+
+
+
+//        collection.drop();
     }
+
+
+    public static Document makeDocument(User user) {
+        return new Document()
+                .append("id", user.getId())
+                .append("name", user.getName())
+                .append("surname", user.getSurname())
+                .append("email", user.getEmail())
+                .append("address", user.getAddress())
+                .append("password", user.getPassword());
+    }
+
+
+    public static <T> void printAllUsers(MongoCollection<T> collection) {
+        for (T t : collection.find()) {
+            System.out.println(t);
+        }
+    }
+
+
+
+
 }
--- Ex_MongoDb01/src/main/java/lt/verbus/model/User.java
+++ Ex_MongoDb01/src/main/java/lt/verbus/model/User.java
@@ -1,4 +1,92 @@
 package lt.verbus.model;
 
 public class User {
+    private long id;
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+    private String address;
+    private String password;
+
+    private User(){
+    }
+
+    public static class Builder {
+        private long id;
+        private String name;
+        private String surname;
+        private int age;
+        private String email;
+        private String address;
+        private String password;
+
+        public Builder(long id) {
+            this.id = id;
+        }
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;  //By returning the builder each time, we can create a fluent interface.
+        }
+
+        public Builder withSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder atAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder withPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.id = this.id;
+            user.name = this.name;
+            user.surname = this.surname;
+            user.email = this.email;
+            user.address = this.address;
+            user.password = this.password;
+            return user;
+        }
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
