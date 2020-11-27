9f9fc23086e66405311c2fedc8e5d2f83865379e
Sarunas Verbus <sarunas@verbus.lt>
2020-11-27 20:23:42 +0200

    cleanup

---
+++ Ex_MongoDb01/src/main/java/lt/verbus/AppMongoDb01.java
@@ -0,0 +1,13 @@
+package lt.verbus;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMongoDb01
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Ex_MongoDb01/src/main/java/lt/verbus/model/User.java
@@ -0,0 +1,4 @@
+package lt.verbus.model;
+
+public class User {
+}
